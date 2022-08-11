fdd54e8a32f18bd36fa710dbd0e239473101413c
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-08-11 16:44:07 +0800

    使用channel来实现互斥锁

--- 01_channel/1_communication/main.go
+++ 01_channel/1_communication/main.go
@@ -10,7 +10,32 @@ func main() {
 	//sendBeforeReceive()
 	//receiveBeforeSend()
 	//testSendAndGetData()
-	signalPattern()
+	//signalPattern()
+	ch := make(chan int) // Create a new channel.
+	go generate(ch)      // Start generate() as a goroutine.
+	for {
+		prime := <-ch
+		fmt.Print(prime, " ")
+		ch1 := make(chan int)
+		go filter(ch, ch1, prime)
+		ch = ch1
+	}
+}
+
+func generate(ch chan int) {
+	for i := 2; ; i++ {
+		ch <- i
+	}
+}
+
+func filter(in, out chan int, prime int) {
+
+	for {
+		i := <-in // Receive value of new variable 'i' from 'in'.
+		if i%prime != 0 {
+			out <- i // Send 'i' to channel 'out'.
+		}
+	}
 }
 
 func signalPattern() {
